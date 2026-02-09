059cd1f41ab1b71d5b3d05680ff26be6bd1b35a8
Fredy BackSlash <fredy31415@gmail.com>
2026-02-09 14:14:53 +0200

    update reamde

--- upload.sh
+++ upload.sh
@@ -1,2 +1,1 @@
-~/arduino-ide/arduino-cli upload --fqbn esp32:esp32:esp32c3  -v -i  ~/Arduino/Girlianda/build/esp32.esp32.esp32c3/Girlianda.ino.bin --port /dev/ttyACM0
-
+~/arduino-ide/arduino-cli upload --fqbn esp32:esp32:esp32c3 -p /dev/ttyACM0  -v --input-file  ./build/esp32.esp32.esp32c3/Girlianda.ino.bin
