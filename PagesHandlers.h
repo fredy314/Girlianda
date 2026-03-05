755198d8d81607d14696e54921f9199cc637d2b7
Fredy BackSlash <fredy31415@gmail.com>
2026-03-05 08:08:17 +0200

    Add Author email

--- PagesHandlers.h
+++ PagesHandlers.h
@@ -1,6 +1,6 @@
 /*
  * Girlianda project
- * Copyright (c) 2026 Fredy
+ * Copyright (c) 2026 Fedir Vilhota <fredy31415@gmail.com>
  * This software is released under the MIT License.
  * See the LICENSE file in the project root for full license information.
  */
